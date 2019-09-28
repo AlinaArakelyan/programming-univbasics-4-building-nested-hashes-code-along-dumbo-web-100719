fc4bc0c193dca01e624ca5078e605139917aa5eb
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-28 00:19:14 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,8 +24,8 @@ def bonus
 
 
   #code your solution here: 
-if epic_tragedy.nil?
-epic_tragedy[:montague][:hero][:status] = "dead"
+
+epic_tragedy[:montague][:hero][:status].nil? = "dead"
 epic_tragedy[:capulet][:herione][:status] = "dead"
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
